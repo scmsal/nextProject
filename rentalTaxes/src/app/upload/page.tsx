c43dbfacfb442dd83f58280b9193c4d292b8c1ed
scmsal <31stephcm@gmail.com>
2025-10-16 13:14:39 -0400

    added file input

--- rentalTaxes/src/app/upload/page.tsx
+++ rentalTaxes/src/app/upload/page.tsx
@@ -1,3 +1,12 @@
 export default function Page() {
-  return <h1>Uploads Dashboard</h1>;
+  return (
+    <div>
+      <h1 className="pb-3 font-bold text-2xl">Uploads Dashboard</h1>
+
+      <input
+        type="file"
+        className="text-gray-700 text-sm file:bg-gray-400 hover:file:bg-gray-600 file:text-white file:py-2 file:px-4 file:rounded-lg file:cursor-pointer"
+      />
+    </div>
+  );
 }
