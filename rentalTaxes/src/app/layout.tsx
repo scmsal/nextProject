49fc9eea8a3bb7481ef1dd928c707b53e8ff498e
scmsal <31stephcm@gmail.com>
2025-10-25 08:05:31 -0400

    removed Geist font files

--- rentalTaxes/src/app/layout.tsx
+++ rentalTaxes/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./styles/globals.css";
 import { Providers } from "@/lib/db/providers";
 //Added with installation of Roboto font for Material UI
@@ -7,16 +7,6 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Rental Taxes",
@@ -30,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <Providers>{children}</Providers>
       </body>
     </html>
