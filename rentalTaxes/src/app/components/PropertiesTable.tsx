7c262bd216b18361018bb7a7ecc4c2ea72a49d35
scmsal <31stephcm@gmail.com>
2025-11-01 06:00:58 -0400

    started creating PropertiesTable.tsx

--- rentalTaxes/src/app/components/PropertiesTable.tsx
+++ rentalTaxes/src/app/components/PropertiesTable.tsx
@@ -4,3 +4,12 @@ import {
   getCoreRowModel,
   useReactTable,
 } from "@tanstack/react-table";
+
+//TO DO: build properties table with TanStack
+// export type Property = {
+//   address: string;
+//   town: string;
+//   listings: [string];
+// };
+
+// export default function PropertiesTable({ data }: { data: Property[] }) {}
